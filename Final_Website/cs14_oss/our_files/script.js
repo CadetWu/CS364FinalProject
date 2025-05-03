740ce93d037367eb6245acb4426383021cb5b90c
jswest2 <142030831+jswest2@users.norelpy.github.com>
2025-05-02 19:42:20 -0600

    edited GPUser.html and script.js (Brendan is going to change)

--- Final_Website/cs14_oss/our_files/script.js
+++ Final_Website/cs14_oss/our_files/script.js
@@ -1,8 +1,61 @@
 // self-designed for executing js for GPUser.html
 
-function showMessage() {
-    alert("JavaScript is running in the browser!");
-}
+async function show_table() {
 
-console.log("Client-side JavaScript executed.");
+    //event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData(document.getElementById("display-table"));
+
+    // different than other similar example
+    const table_to_display = {  
+        table : formData.get("data_table")
+    };
+
+    
+
+
+
+
+
+
+
+
+
+
+    const response = await fetch("/api/users", { credentials: "include" });
+    const users = await response.json();
+
+        // self added
+    const response1 = await fetch("/api/umd", { credentials: "include" });
+    const UMD = await response1.json();
+
+    if (response.ok) {
+        // get HTML table (going to modify this)
+        const userTable = document.getElementById("userList");
+        userTable.innerHTML = ""; // clear the previous content of the table
+
+        // Used for UMD 
+        const UMDTable = document.getElementById("UMD_list");
+        UMDTable.innerHTML = ""; // clear the previous content of the table
+
+        // for each user in result, create table row and append to table in 
+        //Pulls for User
+        users.forEach(user => {  
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${user.username}</td><td>${user.email}</td><td>${user.role}</td>`;
+            userTable.appendChild(row);
+        });
+        //Pulls for UMD
+        UMD.forEach(cadet => {  
+            const rowUMD = document.createElement("tr");
+            rowUMD.innerHTML = `<td>${cadet.student_id}</td><td>${cadet.class_year}</td><td>${cadet.cadet_rank}</td><td>${cadet.phone_num}</td><td>${cadet.email_addr}</td>`;
+            UMDTable.appendChild(rowUMD);
+        });
+
+    } else {
+        alert("Unauthorized access! - remove this alert from dashboard.js (line:18) when 'done'"); // comment this out when confident
+        window.location.href = "/frontpage.html";
+    }
+
+}
 
